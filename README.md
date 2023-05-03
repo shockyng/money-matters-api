960fc694e58c88052c37288b9fab653e771565cf
mdouglas <mdouglaslopess@gmail.com>
2023-05-03 14:55:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Run postgres in docker
+docker run --name money-matters -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=money_matters -p 5433:5432 -d postgres
---
+++ src/main/java/com/moneymatters/MoneyMattersApplication.java
@@ -0,0 +1,13 @@
+package com.moneymatters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMattersApplication.class, args);
+	}
+
+}
