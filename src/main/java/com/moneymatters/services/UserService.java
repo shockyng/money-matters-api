01a170e02491fb1a5c0802d6199579826079c743
mdouglas <mdouglaslopess@gmail.com>
2023-05-04 16:42:40 -0300

    feat: user crud basic

--- src/main/java/com/moneymatters/services/UserService.java
+++ src/main/java/com/moneymatters/services/UserService.java
@@ -1,5 +1,6 @@
 package com.moneymatters.services;
 
+import com.moneymatters.dtos.UserDto;
 import com.moneymatters.models.User;
 import com.moneymatters.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,5 +11,34 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public User storeUser(UserDto userDto) {
+        User user = createUserReceivingDto(userDto);
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, UserDto userDto) {
+        User user = userRepository.findById(id).get();
+
+        user.setUsername(userDto.getUsername());
+        user.setFirstname(userDto.getFirstname());
+        user.setLastname(userDto.getLastname());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+
+        return userRepository.save(user);
+    }
+
+    private User createUserReceivingDto(UserDto userDto) {
+        User user = new User();
+
+        user.setUsername(userDto.getUsername());
+        user.setFirstname(userDto.getFirstname());
+        user.setLastname(userDto.getLastname());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+
+        return user;
+    }
 
 }
