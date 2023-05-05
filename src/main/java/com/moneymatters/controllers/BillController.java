f3052257907b787ab334300734def14a547c331f
geraldodf <geraldodarosfilho@hotmail.com>
2023-05-05 20:52:40 +0200

    Resolving conflicts and adding Bill feature

--- src/main/java/com/moneymatters/controllers/BillController.java
+++ src/main/java/com/moneymatters/controllers/BillController.java
@@ -1,5 +1,51 @@
 package com.moneymatters.controllers;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moneymatters.dtos.BillDto;
+import com.moneymatters.models.Bill;
+import com.moneymatters.services.BillService;
+
+
+@RestController
+@RequestMapping("/bills")
 public class BillController {
+
+    private final BillService billService;
+
+    @Autowired
+    public BillController(BillService billService){
+        this.billService = billService;
+    }
+
+    @GetMapping
+    public ArrayList<Bill> getAll() {
+        return (ArrayList<Bill>) billService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Bill getById(@PathVariable("id") Long id) {
+        return billService.getById(id);
+    }
+
+    @PostMapping
+    public Bill store(@RequestBody BillDto billDto) {
+        return billService.store(billDto);
+    }
+
+    @PutMapping("/{id}")
+    public Bill update(@PathVariable("id") Long id, @RequestBody BillDto billDto) {
+        return billService.update(id, billDto);
+    }
+
     
 }
--- src/main/java/com/moneymatters/models/Bill.java
+++ src/main/java/com/moneymatters/models/Bill.java
@@ -1,5 +1,45 @@
 package com.moneymatters.models;
 
+import java.sql.Date;
+
+import jakarta.validation.constraints.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "bills")
 public class Bill {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bill_id")
+    private Long id;
+
+    @Column(name = "bill_price")
+    private Double price;
+
+    //@Size(min = 2, message = "user name should have at least 2 characters")
+    @Column(name = "bill_name")
+    private String name;
+
+    @Column(name = "bill_description")
+    private String description;
+
+    @Column(name = "bill_payment_type")    
+    private String paymentType;
+
+    @Column(name = "bill_installments")
+    private Integer installments;
+
+    @Column(name = "bill_due_date")
+    private Date dueDate;
     
+
 }
