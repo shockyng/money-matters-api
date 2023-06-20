d95b42d5a2a6a12a35fdacb70e1c512544575413
mdouglasLS <mdouglaslopess@gmail.com>
2023-06-20 13:41:58 -0300

    fix: rename migrations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Run postgres in docker
+```bash
 docker run --name money-matters -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=money_matters -p 5433:5432 -d postgres
+```
--- src/main/java/com/moneymatters/data/models/Role.java
+++ src/main/java/com/moneymatters/data/models/Role.java
@@ -12,6 +12,7 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Long id;
 
     @Column(name = "role_name")
